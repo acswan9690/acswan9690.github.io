08be12935b7b632b82b2dea8b05e6b320c942720
Adam Swan <acswan9690@gmail.com>
2021-10-26 15:47:09 -0500

    template update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Tessellate by HTML5 UP
+	Photon by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -11,10 +11,12 @@
 
 	// Breakpoints.
 		breakpoints({
-			wide:    [ '1281px',  '1680px' ],
-			normal:  [ '1001px',  '1280px' ],
-			narrow:  [ '737px',   '1000px' ],
-			mobile:  [ null,      '736px'  ]
+			xlarge:   [ '1141px',  '1680px' ],
+			large:    [ '981px',   '1140px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '321px',   '480px'  ],
+			xxsmall:  [ null,      '320px'  ]
 		});
 
 	// Play initial animations on page load.
